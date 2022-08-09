6717522ff66a7257912419b9a2c1e20f4a58530b
pl1824 <patricktonylee@gmail.com>
2022-08-09 16:18:28 -0400

    init/added junk and spinner

---
+++ spinner/spinner.go
@@ -0,0 +1,48 @@
+package spinner
+
+import (
+    "fmt"
+    "os"
+    "time"
+)
+
+func Spin(c chan string){
+    current := 0
+    speed := time.Duration(300) // ms
+
+    pattern := "~o~"
+    back := "-----------"
+
+    for {
+        select{
+            case <-c: // handle and print error in future for debug
+                return
+
+            default:
+                // from the right
+                if(current < len(pattern)){
+                    fmt.Printf("%s%s", pattern[len(pattern)-current:], back[current:])
+                // over the left
+                } else if(current == len(back)){
+                    for i := 0; i < len(pattern); i++{
+                        dif := len(pattern)-i
+                        fmt.Printf("%s%s", back[:current - dif], pattern[:dif])
+                        time.Sleep(speed * time.Millisecond)
+                        fmt.Fprintf(os.Stdout, "\r")
+                    }
+                    current = 0
+                    continue
+
+                // moving in the mid
+                } else {
+                     fmt.Printf("%s%s%s", back[:current-len(pattern)], pattern, back[current:])
+                }
+
+                current = current % len(back)
+                current++
+
+                fmt.Fprintf(os.Stdout, "\r")
+                time.Sleep(speed * time.Millisecond)
+        }
+    }
+}
